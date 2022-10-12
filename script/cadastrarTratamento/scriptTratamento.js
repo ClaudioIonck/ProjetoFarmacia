82d0c945384be7b6b2f741e8e58094cdf986e439
ClaudioIonck <claudioluiz180701@gmail.com>
2022-10-12 00:15:36 -0300

    Inputs

---
+++ script/cadastrarTratamento/scriptTratamento.js
@@ -0,0 +1,18 @@
+// Extend the default picker options for all instances.
+$.extend($.fn.datepicker.defaults, {
+    monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre',
+    'Novembre', 'Décembre'],
+    weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    today: 'aujourd\'hui',
+    clear: 'effacer',
+    formatSubmit: 'yyyy/mm/dd'
+  })
+  
+  // Or, pass the months and weekdays as an array for each invocation.
+  $('.datepicker').datepicker({
+    monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre',
+    'Novembre', 'Décembre'],
+    weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    clear: 'effacer',
+    formatSubmit: 'yyyy/mm/dd'
+  })
